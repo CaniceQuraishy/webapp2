00b9750eb18f28d7abae99e93a5682759772b0e3
Canice Musa Quraishy <127295307+CaniceMQ@users.noreply.github.com>
2024-07-09 00:22:23 +0300

    Updated CSS and Adjusted Navbar

--- navbar.php
+++ navbar.php
@@ -67,7 +67,7 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item mx-2">
-            <a class="nav-link active" aria-current="page" href="index.php#home">Home</a>
+            <a class="nav-link active" aria-current="page" href="homepage.php#home">Home</a>
           </li>
           <li class="nav-item mx-2">
             <a class="nav-link" href="about.php#about">About</a>
