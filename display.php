bf7a0a5e18a7ebb44629ca9086b893bc63e2af52
Canice Musa Quraishy <127295307+CaniceMQ@users.noreply.github.com>
2024-07-09 03:10:39 +0300

    Updated Admin Panel

--- display.php
+++ display.php
@@ -6,17 +6,35 @@ $query = "SELECT * FROM form";
 $data = mysqli_query($conn, $query);
 
 $total = mysqli_num_rows($data);
-$result = mysqli_fetch_assoc($data);
 
-echo $result['username']." ".$result['email']." ".$result['password']." ".$result['conpassword'];
+
+
 
 //echo $total';
 
 if($total != 0)
 {
-    //echo "Table has records";
+    ?>
+    <table>
+    <tr>
+        <th> Username</th>
+        <th> Email</th>
+        <th> Password</th>
+</tr>
+
+
+    <?php
+    while ($result = mysqli_fetch_assoc($data))
+    {
+       echo"<tr>
+        <td> ".$result['username']."</td>
+        <td> ".$result['email']."</td>
+        <td> ".$result['password']."</td>
+</tr>";
+    }
 }
 else{
    echo"No records found"; 
 }
 ?>
+</table>
